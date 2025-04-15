6002268f50f9f532948f22083b5b363e939fd132
momi <momi3355@hotmail.com>
2025-04-15 09:55:49 +0900

    프로젝트 생성

---
+++ Shopping/src/main/java/co/yedam/common/FrontController.java
@@ -0,0 +1,39 @@
+package co.yedam.common;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import co.yedam.control.MainControl;
+
+public class FrontController extends HttpServlet {
+
+	Map<String, Control> map;
+
+	public FrontController() {
+		map = new HashMap<>();
+	}
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		map.put("/main.do", new MainControl());
+	}
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) //
+			throws ServletException, IOException {
+		String uri = req.getRequestURI();
+		String context = req.getContextPath();
+		String path = uri.substring(context.length());
+
+		Control sub = map.get(path);
+		sub.exec(req, resp);
+
+	}
+}
---
+++ Shopping/src/test/java/co/yedam/test/ProductList.java
@@ -0,0 +1,9 @@
+package co.yedam.test;
+import java.util.List;
+import java.util.Map;
+
+public class ProductList {
+	public static void main(String[] args) {
+		
+	}
+}
